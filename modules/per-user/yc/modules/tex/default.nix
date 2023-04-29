a28df82ea297e551fbfb92b981a4872a0569e350
Maurice Zhou <yuchen@apvc.uk>
2023-04-29 11:35:21 +0200

    tex: fix dep
    Signed-off-by: Maurice Zhou <yuchen@apvc.uk>

--- modules/per-user/yc/modules/tex/default.nix
+++ modules/per-user/yc/modules/tex/default.nix
@@ -27,7 +27,7 @@ in {
           pdfjam pdfpages
           # code listings
           minted fvextra keyval kvoptions fancyvrb upquote float
-          ifthen calc ifplatform pdftexcmds etoolbox xcolor
+          ifthen ifplatform pdftexcmds etoolbox xcolor
           lineno framed catchfile
           # pictures and tikz
           collection-pictures;
