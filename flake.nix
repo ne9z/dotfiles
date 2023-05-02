71337b0b590a89f042fdd64383a45ceb48703d6d
Maurice Zhou <yuchen@apvc.uk>
2023-05-02 18:53:17 +0200

    unstable small
    Signed-off-by: Maurice Zhou <yuchen@apvc.uk>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    home-manager.url = "github:nix-community/home-manager/release-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
