74886e04a9d35568b2a7b2011eb56f174765588f
Maurice Zhou <yuchen@apvc.uk>
2023-06-01 08:49:28 +0200

    switch to regular channel
    Signed-off-by: Maurice Zhou <yuchen@apvc.uk>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
