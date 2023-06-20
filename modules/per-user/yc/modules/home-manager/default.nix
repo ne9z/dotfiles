7fac7df14f474c8e22fffcdaa874d7f253187ebd
Yuchen Guo <yguo@posteo.net>
2023-06-20 17:34:52 +0200

    add button for suspend
    Signed-off-by: Yuchen Guo <yguo@posteo.net>

--- modules/per-user/yc/modules/home-manager/default.nix
+++ modules/per-user/yc/modules/home-manager/default.nix
@@ -557,6 +557,7 @@ in {
            bindsym --no-warn Mod4+x workspace back_and_forth
            bindsym --no-warn Mod4+y scratchpad show
            bindsym --no-warn Shift+Print exec ${pkgs.grim}/bin/grim
+           bindsym --no-warn Mod4+Shift+l exec ${pkgs.systemd}/bin/systemctl suspend
            bindsym --no-warn Mod4+o exec ${pkgs.foot}/bin/foot ${pkgs.tmux}/bin/tmux attach-session
           }
 
