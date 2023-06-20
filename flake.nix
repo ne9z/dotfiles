23eeada6c9d916820db991084b27adc8abaef1ef
Yuchen Guo <yguo@posteo.net>
2023-06-20 12:49:06 +0200

    unstable
    Signed-off-by: Yuchen Guo <yguo@posteo.net>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
