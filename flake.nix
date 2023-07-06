6018b714ca60c8b523bcb9eb278c5cf13426726a
Yuchen Guo <yguo@posteo.net>
2023-07-06 08:11:38 +0200

    test neovim
    Signed-off-by: Yuchen Guo <yguo@posteo.net>

--- flake.nix
+++ flake.nix
@@ -1,8 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager }:
@@ -14,6 +16,11 @@
             inherit system;
             modules = [
               ./modules
+              (if (builtins.pathExists
+                ./hosts/${hostName}/configuration.nix) then
+                import ./hosts/${hostName}/configuration.nix
+              else
+                { })
               (({ zfs-root, pkgs, lib, ... }: {
                 inherit zfs-root;
                 system.configurationRevision = if (self ? rev) then
@@ -21,12 +28,10 @@
                 else
                   throw "refuse to build: git tree is dirty";
                 system.stateVersion = "23.05";
-
                 imports = [
                   "${nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
                   "${nixpkgs}/nixos/modules/profiles/hardened.nix"
                 ];
-
               }) { inherit zfs-root pkgs lib; })
               home-manager.nixosModules.home-manager
               {
