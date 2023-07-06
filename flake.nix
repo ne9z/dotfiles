daf909ad6bc59ad89653e10be814e82d15c54941
Yuchen Guo <yguo@posteo.net>
2023-07-06 08:03:26 +0200

    more test
    Signed-off-by: Yuchen Guo <yguo@posteo.net>

--- flake.nix
+++ flake.nix
@@ -8,58 +8,43 @@
   outputs = { self, nixpkgs, home-manager }:
     let
       lib = nixpkgs.lib;
-      mkHost = { zfs-root, pkgs, system, ... }:
-        lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./modules
-            (({ zfs-root, pkgs, lib, ... }: {
-              inherit zfs-root;
-              system.configurationRevision = if (self ? rev) then
-                self.rev
-              else
-                throw "refuse to build: git tree is dirty";
-              system.stateVersion = "23.05";
+      mkHost = hostName: system:
+        (({ zfs-root, pkgs, system, ... }:
+          lib.nixosSystem {
+            inherit system;
+            modules = [
+              ./modules
+              (({ zfs-root, pkgs, lib, ... }: {
+                inherit zfs-root;
+                system.configurationRevision = if (self ? rev) then
+                  self.rev
+                else
+                  throw "refuse to build: git tree is dirty";
+                system.stateVersion = "23.05";
 
-              imports = [
-                "${nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
-                "${nixpkgs}/nixos/modules/profiles/hardened.nix"
-              ];
+                imports = [
+                  "${nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
+                  "${nixpkgs}/nixos/modules/profiles/hardened.nix"
+                ];
 
-            }) { inherit zfs-root pkgs lib; })
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-            }
-          ];
-        };
+              }) { inherit zfs-root pkgs lib; })
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+              }
+            ];
+          }) (import ./hosts/${hostName} {
+            system = system;
+            pkgs = nixpkgs.legacyPackages.${system};
+          }));
     in {
       nixosConfigurations = {
-        exampleHost = let
-          system = "x86_64-linux";
-          pkgs = nixpkgs.legacyPackages.${system};
-        in mkHost (import ./hosts/exampleHost { inherit system pkgs; });
-
-        qinghe = let
-          system = "x86_64-linux";
-          pkgs = nixpkgs.legacyPackages.${system};
-        in mkHost (import ./hosts/qinghe { inherit system pkgs; });
-
-        tieling = let
-          system = "x86_64-linux";
-          pkgs = nixpkgs.legacyPackages.${system};
-        in mkHost (import ./hosts/tieling { inherit system pkgs; });
-
-        yinzhou = let
-          system = "x86_64-linux";
-          pkgs = nixpkgs.legacyPackages.${system};
-        in mkHost (import ./hosts/yinzhou { inherit system pkgs; });
-
-        shuiku = let
-          system = "x86_64-linux";
-          pkgs = nixpkgs.legacyPackages.${system};
-        in mkHost (import ./hosts/shuiku { inherit system pkgs; });
+        exampleHost = mkHost exampleHost "x86_64-linux";
+        qinghe = mkHost qinghe "x86_64-linux";
+        tieling = mkHost tieling "x86_64-linux";
+        yinzhou = mkHost yinzhou "x86_64-linux";
+        shuiku = mkHost shuiku "x86_64-linux";
       };
     };
 }
