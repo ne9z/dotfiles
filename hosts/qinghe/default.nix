25b086826aad7685ea6af2edf00550e5f8f98a41
Yuchen Guo <yguo@posteo.net>
2023-07-06 20:21:02 +0200

    mirror
    Signed-off-by: Yuchen Guo <yguo@posteo.net>

--- hosts/qinghe/default.nix
+++ hosts/qinghe/default.nix
@@ -2,7 +2,10 @@
   inherit pkgs system;
   zfs-root = {
     boot = {
-      bootDevices = [ "ata-TOSHIBA_Q300._46DB5111K1MU" ];
+      bootDevices = [
+        "ata-TOSHIBA_Q300._46DB5111K1MU"
+        "ata-Micron_5100_MTFDDAK480TBY_18271D3D0DD1"
+      ];
       availableKernelModules = [ "i915" ];
     };
     networking = {
