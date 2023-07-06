dce458cfc29543d54f9b6c1c7e9c8ae79f99ff5f
Yuchen Guo <yguo@posteo.net>
2023-07-06 07:54:23 +0200

    syntax
    Signed-off-by: Yuchen Guo <yguo@posteo.net>

--- modules/per-user/yc/templates/common/default.nix
+++ modules/per-user/yc/templates/common/default.nix
@@ -11,49 +11,51 @@ in {
     };
   };
   config = mkIf cfg.enable {
-  nixpkgs.config.zathura.useMupdf = true;
+    nixpkgs.config.zathura.useMupdf = true;
 
-  # Enable NetworkManager for wireless networking,
-  # You can configure networking with "nmtui" command.
-  networking.useDHCP = lib.mkDefault true;
-  networking.networkmanager.enable = lib.mkDefault false;
+    # Enable NetworkManager for wireless networking,
+    # You can configure networking with "nmtui" command.
+    networking.useDHCP = lib.mkDefault true;
+    networking.networkmanager.enable = lib.mkDefault false;
 
-  services.openssh = {
-    enable = lib.mkDefault true;
-    settings = { PasswordAuthentication = lib.mkDefault false; };
-  };
+    services.openssh = {
+      enable = lib.mkDefault true;
+      settings = { PasswordAuthentication = lib.mkDefault false; };
+    };
 
-  boot.zfs.forceImportRoot = lib.mkDefault false;
+    boot.zfs.forceImportRoot = lib.mkDefault false;
 
-  nix.settings.experimental-features = lib.mkDefault [ "nix-command" "flakes" ];
+    nix.settings.experimental-features =
+      lib.mkDefault [ "nix-command" "flakes" ];
 
-  programs.git.enable = true;
+    programs.git.enable = true;
 
-  services.logrotate.checkConfig = false;
-  security = {
-    doas.enable = lib.mkDefault true;
-    sudo.enable = lib.mkDefault false;
-  };
+    services.logrotate.checkConfig = false;
+    security = {
+      doas.enable = lib.mkDefault true;
+      sudo.enable = lib.mkDefault false;
+    };
 
-  environment.systemPackages = builtins.attrValues {
-    inherit (pkgs)
-      mg # emacs-like editor
-      parted # other programs
-    ;
-  };
+    environment.systemPackages = builtins.attrValues {
+      inherit (pkgs)
+        mg # emacs-like editor
+        parted # other programs
+      ;
+    };
 
-  security = {
-    allowSimultaneousMultithreading = false;
-    lockKernelModules = false;
-    apparmor.enable = true;
-  };
-  environment.memoryAllocator.provider = "libc";
+    security = {
+      allowSimultaneousMultithreading = false;
+      lockKernelModules = false;
+      apparmor.enable = true;
+    };
+    environment.memoryAllocator.provider = "libc";
 
-  # disable gc which always deletes downloaded nixpkg cache
-  nix = {
-    gc = {
-      automatic = false;
-      options = "--delete-old";
+    # disable gc which always deletes downloaded nixpkg cache
+    nix = {
+      gc = {
+        automatic = false;
+        options = "--delete-old";
+      };
     };
   };
 }
