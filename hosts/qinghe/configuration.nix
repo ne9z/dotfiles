eac9a5c93a89614586b1274acb13b31cadd65a55
Yuchen Guo <yguo@posteo.net>
2023-07-17 05:46:59 +0200

    fix type
    Signed-off-by: Yuchen Guo <yguo@posteo.net>

--- hosts/qinghe/configuration.nix
+++ hosts/qinghe/configuration.nix
@@ -1,5 +1,9 @@
 { ... }: {
   boot.kernelParams = [
-    "radeon.cik_support=0 radeon.si_support=0 amdgpu.cik_support=1 amdgpu.si_support=1 amdgpu.dc=1"
+    "radeon.cik_support=0"
+    "radeon.si_support=0"
+    "amdgpu.cik_support=1"
+    "amdgpu.si_support=1"
+    "amdgpu.dc=1"
   ];
 }
