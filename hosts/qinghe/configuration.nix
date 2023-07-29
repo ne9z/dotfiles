961036353c5c1a9425c0892144b0730e61f530fe
Yuchen Guo <yguo@posteo.net>
2023-07-29 22:46:43 +0200

    move tablet configuration to per-host config
    Signed-off-by: Yuchen Guo <yguo@posteo.net>

---
+++ hosts/qinghe/configuration.nix
@@ -0,0 +1,16 @@
+{ ... }: {
+  home-manager.users.yc.wayland.windowManager.sway.config.input = {
+    "9580:110:PenTablet_Pen" = {
+      # the ratio is 16:9 on the graphic tablet
+      # on a 3840x2160 screen, map to upper-right
+      # multiply by 170
+      # parameters:
+      # offset from upper left
+      # map_to_region = "x_offset y_offset x_map_to y_map_to"
+      map_to_region = "1120 0 2720 1530";
+    };
+    "1386:830:Wacom_Intuos_PT_M_2_Pen" = {
+      map_to_region = "1120 0 2720 1530";
+    };
+  };
+}
