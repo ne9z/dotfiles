5b91c4a32c488d25e0c6d952852b55bcdac3fdf1
Yuchen Guo <yguo@posteo.net>
2023-08-08 23:59:46 +0200

    personal dictionary in enchant
    Signed-off-by: Yuchen Guo <yguo@posteo.net>

--- modules/per-user/yc/modules/home-manager/default.nix
+++ modules/per-user/yc/modules/home-manager/default.nix
@@ -337,6 +337,21 @@ in {
               [main]
               font=sans-serif:size=18:weight=bold'';
           };
+          # Personal word lists are simple plain text files with one
+          # word per line.  Lines starting with a hash sign ‘#’ are
+          # ignored.  See man 5 enchant
+          "enchant/de_DE.dic".text = ''
+            subsection
+            subsubsection
+            begin
+            end
+            proof
+            newpage
+            document
+          '';
+          "enchant/en_US.dic".text = ''
+            Emacs
+          '';
           "w3m/config" = { source = ./not-nix-config-files/w3m-config; };
           "w3m/keymap" = { source = ./not-nix-config-files/w3m-keymap; };
         };
