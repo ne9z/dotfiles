18c47fa3bb9ef5527effc676310a986b51873021
Yuchen Guo <yguo@posteo.net>
2023-08-27 23:10:44 +0200

    nvme
    Signed-off-by: Yuchen Guo <yguo@posteo.net>

--- hosts/yinzhou/configuration.nix
+++ hosts/yinzhou/configuration.nix
@@ -1,5 +1,6 @@
 { pkgs, ... }: {
   imports = [ ../../imports/common.nix ../../imports/desktop.nix ];
+  boot.initrd.availableKernelModules = [ "nvme" ];
   networking = {
     hostName = "yinzhou";
     hostId = "e74b069d";
