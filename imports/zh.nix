f1df39beb9f234663849d52cd8ad46d201018b1b
Yuchen Guo <yguo@posteo.net>
2023-08-28 08:15:43 +0200

    extra group
    Signed-off-by: Yuchen Guo <yguo@posteo.net>

--- imports/zh.nix
+++ imports/zh.nix
@@ -5,10 +5,12 @@
   i18n.inputMethod.enabled = "fcitx5";
   users.mutableUsers = false;
   users.users = {
-    gg = {
+    yc = {
       # "!" means login disabled
       initialHashedPassword = "$6$UxT9KYGGV6ik$BhH3Q.2F8x1llZQLUS1Gm4AxU7bmgZUP7pNX6Qt3qrdXUy7ZYByl5RVyKKMp/DuHZgk.RiiEXK8YVH.b2nuOO/";
       isNormalUser = true;
+      group = "yc";
+      extraGroups = [ "wheel" ];
     };
   };
 }
