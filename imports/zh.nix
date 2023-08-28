5a2a457c82527cef44377e3ab1b10f4b1b5d39f9
Yuchen Guo <yguo@posteo.net>
2023-08-28 08:40:26 +0200

    fix autologin
    Signed-off-by: Yuchen Guo <yguo@posteo.net>

--- imports/zh.nix
+++ imports/zh.nix
@@ -7,7 +7,7 @@
   };
   services.xserver.enable = true;
   services.xserver.displayManager = {
-    autoLogin.user = "yc";
+    autoLogin.user = "mima";
     lightdm.enable = true;
   };
   services.xserver.desktopManager.lxqt.enable = true;
