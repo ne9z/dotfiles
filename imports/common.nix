3cfd935091cb2a9b85e2ff563bf890d1bce4a739
Yuchen Guo <yguo@posteo.net>
2023-09-30 17:47:15 +0200

    use iwd
    Signed-off-by: Yuchen Guo <yguo@posteo.net>

--- imports/common.nix
+++ imports/common.nix
@@ -26,6 +26,7 @@ in {
   systemd.network.wait-online.enable = false;
   networking.useDHCP = false;
   networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.backend = "iwd";
   services = {
     tor = {
       enable = true;
