b152e26e7dab33633e16820fc348153b251fe5ff
Yuchen Guo <yguo@posteo.net>
2023-10-06 09:43:55 +0200

    pdf-tools

--- imports/desktop.nix
+++ imports/desktop.nix
@@ -5,7 +5,7 @@ let
   buildEmacs = (pkgs.emacsPackagesFor pkgs.emacs29-pgtk).emacsWithPackages;
   emacsPkg = buildEmacs (epkgs:
     builtins.attrValues {
-      inherit (epkgs.melpaPackages) nix-mode magit;
+      inherit (epkgs.melpaPackages) nix-mode magit pdf-tools;
       inherit (epkgs.elpaPackages) auctex pyim pyim-basedict;
       inherit (epkgs.treesit-grammars) with-all-grammars;
     });
