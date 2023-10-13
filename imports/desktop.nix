a1a2f4927d0ca6a3456c29d900e5b5452538c1c1
Yuchen Guo <yguo@posteo.net>
2023-10-13 12:03:20 +0200

    fix path

--- imports/desktop.nix
+++ imports/desktop.nix
@@ -256,27 +256,27 @@ in {
       options = [ "rw" "size=1G" "uid=yc" "gid=users" "mode=1700" ];
     };
     "/home/yc/Downloads" = {
-      device = "/oldroot/home/Downloads";
+      device = "/oldroot/home/yc/Downloads";
       fsType = "none";
       options = homeBindOpts;
     };
     "/home/yc/Documents" = {
-      device = "/oldroot/home/Documents";
+      device = "/oldroot/home/yc/Documents";
       fsType = "none";
       options = homeBindOpts;
     };
     "/home/yc/nixos-config" = {
-      device = "/oldroot/home/nixos-config";
+      device = "/oldroot/home/yc/nixos-config";
       fsType = "none";
       options = homeBindOpts;
     };
     "/home/yc/.gnupg" = {
-      device = "/oldroot/home/.gnupg";
+      device = "/oldroot/home/yc/.gnupg";
       fsType = "none";
       options = homeBindOpts;
     };
     "/home/yc/.password-store" = {
-      device = "/oldroot/home/.password-store";
+      device = "/oldroot/home/yc/.password-store";
       fsType = "none";
       options = homeBindOpts;
     };
