d654ff506b5903c4fc1a96bd2b06ed87a5fa0457
Yǔchēn Guō 郭宇琛 <yguo@posteo.net>
2023-10-24 13:09:10 +0200

    enable luks

--- hosts/qinghe/default.nix
+++ hosts/qinghe/default.nix
@@ -3,9 +3,9 @@
   zfs-root = {
     boot = {
       bootDevices = [
-        "ata-TOSHIBA_Q300._46DB5111K1MU"
         "ata-Micron_5100_MTFDDAK480TBY_18271D3D0DD1"
       ];
+      luks.enable = true;
     };
   };
 }
