c6b918f6cccb033377e52b6cc454ecb8111852c1
Yǔchēn Guō 郭宇琛 <yguo@posteo.net>
2023-11-16 15:53:18 +0100

    low res laptop

--- hosts/yinzhou/configuration.nix
+++ hosts/yinzhou/configuration.nix
@@ -4,7 +4,10 @@ in {
   imports =
     [ ../../imports/common.nix ../../imports/desktop.nix ../../imports/yc.nix ];
   time.timeZone = "Europe/Berlin";
-  boot.initrd.availableKernelModules = [ "nvme" ];
+  environment.sessionVariables = {
+    QT_WAYLAND_FORCE_DPI = "96";
+    GDK_DPI_SCALE = "1";
+  };
   networking = {
     hostName = "yinzhou";
     hostId = "e74b069d";
