06bf3e55b299969b7513ebd5ddf0d00886f9e34d
Yǔchēn Guō 郭宇琛 <yguo@posteo.net>
2023-11-18 18:40:37 +0100

    add tmpfile rule for watch dir

--- imports/server.nix
+++ imports/server.nix
@@ -77,6 +77,8 @@
     "net.core.wmem_max" = 1048576;
     "fs.file-max" = 65536;
   };
+  # clean up watch dir after one day
+  systemd.tmpfiles.rules = [ "d '/home/our/新种子' 0755 our users 1d" ];
   systemd.services.rtorrent.serviceConfig.LimitNOFILE = 10240;
   services.rtorrent = {
     enable = true;
