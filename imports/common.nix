231300dbead2961bf3ec77cf88f7a532bd3488a5
Yǔchēn Guō 郭宇琛 <yguo@posteo.net>
2023-11-22 00:06:26 +0100

    more fixes
    Signed-off-by: Yǔchēn Guō 郭宇琛 <yguo@posteo.net>

--- imports/common.nix
+++ imports/common.nix
@@ -19,8 +19,8 @@ in {
     "$6$UxT9KYGGV6ik$BhH3Q.2F8x1llZQLUS1Gm4AxU7bmgZUP7pNX6Qt3qrdXUy7ZYByl5RVyKKMp/DuHZgk.RiiEXK8YVH.b2nuOO/";
 
   networking = {
-    useDHCP = true;
-    useNetworkd = true;
+    useDHCP = mkDefault true;
+    useNetworkd = mkDefault true;
     hosts = { "200:8bcd:55f4:becc:4d85:2fa6:2ed2:5eba" = [ "tl.yc" ]; };
     nameservers = [ "::1" ];
   };
