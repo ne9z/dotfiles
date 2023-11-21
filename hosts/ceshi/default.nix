3ff422a92dda2626db12cad7bde09899c0bb704a
Yǔchēn Guō 郭宇琛 <yguo@posteo.net>
2023-11-22 00:35:29 +0100

    scale to 3 for hidpi
    Signed-off-by: Yǔchēn Guō 郭宇琛 <yguo@posteo.net>

--- hosts/ceshi/default.nix
+++ hosts/ceshi/default.nix
@@ -1,4 +1,8 @@
 {
   imports = [ ./configuration.nix ];
   zfs-root = { boot = { enable = false; }; };
+  environment.sessionVariables = {
+    QT_WAYLAND_FORCE_DPI = "288";
+    GDK_DPI_SCALE = "3";
+  };
 }
