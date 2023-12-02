5e267257ba5e49218884f73d7d3cdca279fa76f6
Yǔchēn Guō 郭宇琛 <yguo@posteo.net>
2023-12-02 09:34:50 +0100

    fix

--- imports/profiles/live.nix
+++ imports/profiles/live.nix
@@ -20,5 +20,4 @@
   boot.initrd.availableKernelModules = [ "i915" ];
   boot.initrd.systemd.enable = false;
   isoImage.squashfsCompression = "lz4";
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
 }
